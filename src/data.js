4b29ba4e537f21dc901cdb984d25b679d6e33547
Nikita <nikitap47@gmail.com>
2021-09-26 10:46:34 -0700

    Adding Things

---
+++ src/data.js
@@ -0,0 +1,55 @@
+import weather from './assets/Weather.PNG';
+import food from './assets/food-order.PNG';
+import state from './assets/StateUsingRedux.PNG';
+import inventory from './assets/inventory-application.PNG';
+
+export const projects = [
+  {
+    title: "Current Weather",
+    subtitle: "Weather API",
+    description:
+      "The application provides current weather details using an open source weather API.",
+    image: weather,
+    link: "https://nikitap47.github.io/weather/",
+  },
+  {
+    title: "Food Order",
+    subtitle: "React and Firebase",
+    description:
+      "Using the application a customer can order items from the menu to their address.",
+    image: food,
+    link: "https://nikitap47.github.io/food-order/",
+  },
+  {
+    title: "ReduxProject",
+    subtitle: "State with Redux",
+    description:
+      "In the application various states for input fields and counters are managed using redux.",
+    image: state,
+    link: "https://nikitap47.github.io/state-using-redux/",
+  },
+  {
+    title: "Inventory App",
+    subtitle: "Single Page Application",
+    description:
+      "Using firebase and react router a basic inventory application is built to notify progress of various orders.",
+    image: inventory,
+    link: "https://nikitap47.github.io/inventory-application/",
+  },
+];
+
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "BootStrap",
+  "HTML",
+  "CSS",
+  "PHP",
+  "Tailwind CSS",
+  "Photoshop",
+  "TypeScript",
+  "Rest API",
+  "FireBase",
+  "MongoDB"
+];
